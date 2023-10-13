dc369be92fc4fed12369fcd128e2a4c27a8cf9ae
Alireza forghi <aliwebdevf@gmail.com>
2023-10-13 19:23:23 +0330

    Fix Search option bug

--- src/Components/SearchInput.tsx
+++ src/Components/SearchInput.tsx
@@ -2,16 +2,18 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../client/gameQueryStore";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
 
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
-
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
+        navigate("/");
         if (ref.current) setSearchText(ref.current.value);
       }}
     >
